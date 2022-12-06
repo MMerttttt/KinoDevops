0608dcba5c902ecdbb201707d48f188de8fbbc11
MMerttttt <87767161+MMerttttt@users.noreply.github.com>
2022-12-06 09:14:28 +0100

    program

--- Kinoverwaltung/src/components/App.jsx
+++ Kinoverwaltung/src/components/App.jsx
@@ -7,7 +7,7 @@ import {
 import { Home } from './Home'
 import '../styles/global.css';
 import { CreatePlayer } from './CreatePlayer';
-import { TournamentList } from './TournamentList';
+import { TournamentList } from './Program';
 import { TournamentInformation } from './TournamentInformation';
 
 
